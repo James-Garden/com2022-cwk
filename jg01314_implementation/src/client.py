1e6758b5de078c8571eb9fd8a2d7ce42f37390e9
James Garden <jamesegarden@outlook.com>
2022-05-01 23:29:44 +0100

    finished client interface and sending functions for invites and responses

--- jg01314_implementation/src/client.py
+++ jg01314_implementation/src/client.py
@@ -1,34 +1,46 @@
-import socket
+import asyncio
 import threading
+import websockets
+import time
 import tkinter as tk
+import signal
 from queue import Queue
 
 # Defining constants
 SERVER = ('127.0.0.1', 1234)  # Server hostname and port
-USER = "james_g"              # User login name
-
-# Defining global variables
-invitation_received = False
-server_log = Queue()
+USERNAME = "james_g"              # User login name
+PASSWORD = "garden"
 
 
-# Creating the client GUI
-class Interface(tk.Tk):
+# The program itself
+class App(tk.Tk):
     # Constructor
-    def __init__(self, conn):
+    def __init__(self, user, password):
         super().__init__()
+        self.close_conn = False
+        self.user = user
+        self.password = password
+        # What to do when the user closes the window
+        self.protocol("WM_DELETE_WINDOW", self.close)
+        # Unprocessed messages from the server
+        self.server_log = Queue()
+        # Messages that need to be sent to the server
+        self.outbox = Queue()
         # Displayed log
         self.log = Queue()
-        # Server connection
-        self.sock = conn
         # Window title
         self.title("Event Planning Protocol - Client")
         # Welcome message
-        self.greeting = tk.Label(text=f"Welcome, {USER}", )
+        self.greeting = tk.Label(text=f"Welcome, {USERNAME}", )
         # Text entry box for sending an invitation
         self.send_inv_entry = tk.Entry(width=50, justify='center')
         # Button to send invitation
-        self.send_inv_button = tk.Button(width=15, height=3, command=self.send_invitation, text="Send Invitation")
+        self.send_inv_button = tk.Button(
+            width=15,
+            height=3,
+            text="Send Invitation",
+            command=self.send_invite
+        )
         # Box containing message from a currently active invitation
         self.active_inv_text = tk.Entry(width=50, justify='center')
         self.active_inv_text.insert('end', "No Active Invitation")
@@ -36,9 +48,21 @@ class Interface(tk.Tk):
         # Buttons frame
         self.button_frame = tk.Frame()
         # Button to accept an invitation
-        self.yes_button = tk.Button(self.button_frame, width=8, height=2, command=self.accept_invite, text="Yes")
+        self.yes_button = tk.Button(
+            self.button_frame,
+            width=8,
+            height=2,
+            text="Yes",
+            command=self.accept_invite
+        )
         # Button to reject an invitation
-        self.no_button = tk.Button(self.button_frame, width=8, height=2, command=self.reject_invite, text="No")
+        self.no_button = tk.Button(
+            self.button_frame,
+            width=8,
+            height=2,
+            text="No",
+            command=self.reject_invite
+        )
         # Label for server response log
         self.log_label = tk.Label(text="Server Log", )
         # Log of server responses
@@ -53,36 +77,50 @@ class Interface(tk.Tk):
         self.no_button.pack(side='right')
         self.log_label.pack()
         self.log_text.pack()
-        self.after(0, self.refresh)  # Start refreshing
+        # Start the connection thread
+        self.conn_thread = threading.Thread(target=self._connection_thread)
+        self.conn_thread.daemon = True
+        self.conn_thread.start()
+        # Start refreshing the GUI
+        self.call_id = self.after(0, self._refresh)
+
+    # Do something when the user closes the interface
+    def close(self):
+        self.after_cancel(self.call_id)
+        self.close_conn = True
+        self.destroy()
+        time.sleep(0.2)
+        print("Goodbye.")
 
     # Send an invitation to the server
-    def send_invitation(self):
-        msg = "Invitation:"+self.send_inv_entry.get()
-        self.sock.sendall(msg.encode())
+    def send_invite(self):
+        msg = "P_GO_OUT:"+self.send_inv_entry.get()
+        self._send(msg)
 
     # Accept/Reject an invitation from another user
     def accept_invite(self):
-        pass
+        msg = "P_YES"
+        self._send(msg)
 
     def reject_invite(self):
-        pass
+        msg = "P_NO"
+        self._send(msg)
 
     # Check for messages from the server
-    def refresh(self):
-        global server_log
-        if server_log.empty():  # If there is nothing to be read from the log of server messages
-            self.after(10, self.refresh)  # Wait 0.5s then check again
+    def _refresh(self):
+        if self.server_log.empty():  # If there is nothing to be read from the log of server messages
+            self.call_id = self.after(10, self._refresh)  # Wait 0.5s then check again
         else:  # If there are messages waiting to be read
-            data = server_log.get()
+            data = self.server_log.get()
             self._update_log(data)  # Update the log section of the client
-            self._check_for_invite(data)  # Check if an invitation has been received
+            self._process_msg(data)  # Check if an invitation has been received
 
-            self.after(10, self.refresh)  # Wait 0.01s then check again
+            self.call_id = self.after(10, self._refresh)  # Wait 0.01s then check again
 
+    # Update the server log output text area
     def _update_log(self, data):
-        msg = "[SERVER] " + data.decode() + "\n"
+        msg = f"[{time.strftime('%X')}] " + data.decode() + "\n"
         if self.log.qsize() == 10:  # If the output area of the client is full
-            self.log_text.delete('1.0', tk.END)  # Clear the output area
             self.log.get()  # Remove the oldest message from the queue
             self.log.put(msg)
             for i in range(0, 10):
@@ -92,45 +130,76 @@ class Interface(tk.Tk):
             pos = str(self.log.qsize() + 1) + '.0'
             self.log_text.insert(pos, msg)
 
-    def _check_for_invite(self, data):
+    # Do something depending on what the server sends
+    def _process_msg(self, data):
         data = data.decode().split(":")
         msg_type, msg = data[0], ''.join(data[1:])
-        if msg_type == "Invitation":
+        if msg_type == "P_GO_OUT":
             self.active_inv_text.config(state='normal')
             self.active_inv_text.delete('0', tk.END)
             self.active_inv_text.insert('end', msg)
             self.active_inv_text.config(state='readonly')
 
+    # Put whatever data needs to be sent into the outbox
+    def _send(self, data):
+        payload = data.encode()
+        self.outbox.put(payload)
 
-# Thread which just awaits data from server
-class ReceiveThread(threading.Thread):
-    def __init__(self, conn):
-        super().__init__()
-        self.sock = conn
+    # CONNECTION MANAGER
+    # The thread that will be run when the app is created
+    def _connection_thread(self):
+        asyncio.run(self._start_connection())
 
-    def run(self):
-        while True:
-            data = self.sock.recv(1024)
-            print("Received data:", data.decode())
-            server_log.put(data)
+    async def _start_connection(self):
+        try:
+            # Try to start the connection
+            print("Connecting to server...")
+            async with websockets.connect(f"ws://{SERVER[0]}:{SERVER[1]}") as websocket:
+                print("Connected.")
+                asyncio.create_task(self._recv_loop(websocket))  # Starting listening loop
+                asyncio.create_task(self._send_loop(websocket))  # Start sending loop
+
+                # await recv_loop_task  # Wait for the recv loop to finish (i.e. when disconnecting)
+                while not self.close_conn:
+                    await asyncio.sleep(0.1)
+
+                print("Disconnecting...")
+
+        except OSError:
+            print(f"Error: Unable to connect to server at {SERVER[0]}:{SERVER[1]} ")
+            self.close()
+
+    async def _recv_loop(self, websocket):
+        try:
+            while True:
+                data = await websocket.recv()
+                print("Received data:", data.decode(), f"[RAW DATA:{data}]")
+                self.server_log.put(data)
+        except:  # While you shouldn't use bare "except" statements, for some reason ConnectionClosedOK is not valid
+            pass
+
+    async def _send_loop(self, websocket):
+        try:
+            while True:
+                if self.outbox.empty():
+                    await asyncio.sleep(1)
+                    continue
+                while not self.outbox.empty():
+                    payload = self.outbox.get()
+                    await websocket.send(payload)
+        except ConnectionError:
+            pass
+
+
+def handler():
+    app.close()  # Close the program gracefully
 
 
 if __name__ == '__main__':
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create the socket
-    try:
-        sock.connect(SERVER)  # Connect to the server
-        sock.send(b"")
-        recv_thread = ReceiveThread(sock)  # Create a thread for receiving packets
-        recv_thread.daemon = True  # The receiving thread should close when the program does, else it would loop forever
-        recv_thread.start()  # Listen for packets from the server
-
-        root = Interface(sock)  # Create the GUI
-        root.mainloop()  # Launch the GUI
-
-        sock.close()  # Close the connection to the server when the user closes the tkinter window
-    except TimeoutError:
-        print(f"Error: Connection to server at {SERVER[0]}:{SERVER[1]} timed out")
-        input("Press ENTER to quit")
-    except InterruptedError:
-        print("Error: Connection to server interrupted")
-        input("Press ENTER to quit")
+    # If the program is forcibly stopped using CTRL + C in the python shell itself or STOP on an IDE such as pyCharm,
+    # the program should close gracefully
+    signal.signal(signal.SIGINT, handler)
+    # Create the app and connect to the server
+    app = App(USERNAME, PASSWORD)
+    # Show the GUI
+    app.mainloop()
